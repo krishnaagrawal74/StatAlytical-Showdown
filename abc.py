496b25d6b41e0e2df71038f4e7c79c5a68f15285
Krishna Raj Agrawal <krishnarajagrawal@gmail.com>
2025-03-25 17:40:32 +0530

    Delete abc.py

--- abc.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-from scipy import stats
-
-file_path = 'statalyticalshowdownCSV.csv'  
-df = pd.read_csv(file_path)
